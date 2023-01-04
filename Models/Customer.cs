0c2d5f2a8ed35adc08d5a0ad870959438f17deed
Javier De Jesus <ismael.dt@hotmail.com>
2023-01-04 18:14:42 -0400

    Agregar archivos de proyecto.

---
+++ Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Schad.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string CustName { get; set; }
+        public string Adress { get; set; }
+        public bool Status { get; set; }
+        public int CustomerTypeId { get; set; }
+    }
+}
